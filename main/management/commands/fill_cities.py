791d3b0aa56f2ca3a2b20c246a1ecba4a6dab2e2
Mik <kimsergkorea@gmail.com>
2024-07-20 11:32:45 +0900

    silent UNIQUE constrain exception on fill_db

--- main/management/commands/fill_cities.py
+++ main/management/commands/fill_cities.py
@@ -6,4 +6,7 @@ from scripts.fill_cities import fill_out_db
 class Command(BaseCommand):
     '''Command to populate City model from json file using script in scripts/fill_cities module'''
     def handle(self, *args, **options):
-        fill_out_db()
+        try:
+            fill_out_db()
+        except Exception:
+            return 0
